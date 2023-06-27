0282fc398a284e260047ab4f482a4e1cbf7c01d7
anotherinthefire <rongodfreyultra@gmail.com>
2023-06-27 12:38:13 +0800

    team

---
+++ src/data/team.js
@@ -0,0 +1,47 @@
+import ultra from '../assets/profile/ultra.jfif'
+import solitario from '../assets/profile/solitario.jfif'
+import garcia from '../assets/profile/garcia.jfif'
+import casal from '../assets/profile/casal.jpg'
+import escobar from '../assets/profile/escobar.jfif'
+import rivera from '../assets/profile/rivera.jfif'
+
+const team = [
+    {
+        avatar: rivera,
+        name: "Kurt Christian Edrian Rivera",
+        title: "UI/UX Designer",
+        linkedin: "https://www.linkedin.com/in/kurt-rivera-31aa23273/",
+    },
+    {
+        avatar: escobar,
+        name: "Benedict Escobar",
+        title: "Front End Developer",
+        linkedin: "https://www.linkedin.com/in/benedict-c-escobar-93b486259/",
+    },
+    {
+        avatar: casal,
+        name: "Jimwell Casal",
+        title: "Network Engineer",
+        linkedin: "https://www.facebook.com/jmwll.csl.25",
+    },
+    {
+        avatar: garcia,
+        name: "Jhonil Garcia",
+        title: "Tech Support",
+        linkedin: "https://www.linkedin.com/in/jhonil-garcia-b99552275/",
+    },
+    {
+        avatar: solitario,
+        name: "Elaijha Luis Solitario",
+        title: "Back End Developer",
+        linkedin: "https://www.linkedin.com/in/elaijha-luis-solitario-440656275/",
+    },
+    {
+        avatar: ultra,
+        name: "Ron Godfrey Ultra",
+        title: "Project Manager, Database Designer",
+        linkedin: "https://www.linkedin.com/in/ron-godfrey-ultra-036298241/",
+    },
+]
+
+export default team
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import hero from '../assets/human.png'
 import Menu from '../components/Menu'
 import contactMethods from "../data/contactMethods";
 import footerNavs from "../data/HomeNavs.js"
+import team from "../data/team";
 
 const Home = () => {
   const handleClickScroll = (id) => {
@@ -127,8 +128,43 @@ const Home = () => {
         </div>
       </section>
 
-      <section id="About" className="h-screen">
-        <h1>About</h1>
+      <section id="About" className="h-screen py-14">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+          <div className="max-w-xl">
+            <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+              Meet our team
+            </h3>
+            <p className="text-gray-600 mt-3">
+              3rd Year Bachelor of Science in Information Technology Students from Quezon City University, SBIT-3B.
+            </p>
+          </div>
+          <div className="mt-12">
+            <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              {
+                team.map((item, idx) => (
+                  <li key={idx} className="flex gap-4 items-center">
+                    <div className="flex-none w-24 h-24">
+                      <img
+                        src={item.avatar}
+                        className="w-full h-full rounded-full"
+                        alt=""
+                      />
+                    </div>
+                    <div>
+                      <h4 className="text-gray-700 font-semibold sm:text-lg">{item.name}</h4>
+                      <p className="text-indigo-600">{item.title}</p>
+                      <div className="mt-3 flex gap-4 text-gray-400">
+                        <a href={item.linkedin}>
+                          <svg className="w-5 h-5 duration-150 hover:text-gray-500" fill="none" viewBox="0 0 48 48"><g clipPath="url(#clip0_17_68)"><path fill="currentColor" d="M44.447 0H3.544C1.584 0 0 1.547 0 3.46V44.53C0 46.444 1.584 48 3.544 48h40.903C46.407 48 48 46.444 48 44.54V3.46C48 1.546 46.406 0 44.447 0zM14.24 40.903H7.116V17.991h7.125v22.912zM10.678 14.87a4.127 4.127 0 01-4.134-4.125 4.127 4.127 0 014.134-4.125 4.125 4.125 0 010 8.25zm30.225 26.034h-7.115V29.766c0-2.653-.047-6.075-3.704-6.075-3.703 0-4.265 2.896-4.265 5.887v11.325h-7.107V17.991h6.826v3.13h.093c.947-1.8 3.272-3.702 6.731-3.702 7.21 0 8.541 4.744 8.541 10.912v12.572z" /></g><defs><clipPath id="clip0_17_68"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
       </section>
 
       <footer className="pt-10">
@@ -155,10 +191,10 @@ const Home = () => {
               <p className="text-gray-700 font-semibold">Get the app</p>
               <div className="grid grid-cols-2 gap-3 mt-3 sm:block">
                 <div className="mb-3 max-w-44">
-                  <GooglePlayButton url="" theme="light" />
+                  <GooglePlayButton url="" theme="dark" />
                 </div>
                 <div className="mb-3 max-w-44">
-                  <AppStoreButton url="" theme="light" />
+                  <AppStoreButton url="" theme="dark" />
                 </div>
               </div>
 
